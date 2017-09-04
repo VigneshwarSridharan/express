b65b5adf3887f2eb237702bc2da1c1546fb6d51c
vigneshwar <s.vigneshwar@wintlt.com>
2017-09-04 10:59:15 +0530

    user js changes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send("okokoko");
+  res.send("45");
 });
 
 router.post('/', function (req, res) {
